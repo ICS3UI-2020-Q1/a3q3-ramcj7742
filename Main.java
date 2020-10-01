fc6935df6a695fc686037a94e745b6015cf14398
ramcj7742 <ramcj7742@wrdsb.ca>
2020-10-01 15:25:09 +0000

    Finished grading problem

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Sets a number grade to letter grade
+ * @author Jaden Ramcharan
  */
 public class Main {
 
@@ -9,7 +10,34 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Creates scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //gets letter grade from user
+    System.out.println("Please enter a letter grade");
+    String grade = input.nextLine();
+
+    //uses switch case statement to determine number grade
+    switch(grade){
+      case "A":
+       System.out.println("This is between 80% and 100%");
+       break;
+      case "B":
+       System.out.println("This is between 70% and 79%");
+       break;
+      case "C":
+       System.out.println("This is between 60% and 69%");
+       break;
+      case "D":
+       System.out.println("This is between 50% and 59%");
+       break;
+      case "F":
+       System.out.println("This is between 0% and 49%");
+       default:
+       System.out.println("invalid character, make sure letter is uppercase");
+       
+
+    }
     
   }
 }
